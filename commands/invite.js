86f4c4887a5100189759f20ea0d9e0d2ac60d465
Corwnbot <123889850+Corwnbot@users.noreply.github.com>
2023-02-13 09:34:53 +0300

    Add files via upload

---
+++ commands/invite.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const config = require('../config.json');
+
+module.exports.run = async (client, message, args) => {
+    let prefix = config.prefix;
+    if (message.author.bot) return;
+    if(!message.content.startsWith(prefix)) return;
+
+    let invite = new Discord.MessageEmbed()
+    .setTitle("Invite & Support Link!")
+    .addField("Invite Link", "[Click here to invite me](https://discord.com/api/oauth2/authorize?client_id=1017785903927799868&permissions=8&scope=bot%20applications.commands)")
+    .addField("OWNER ID", "(<@1011281371832004622>)")
+    .setTimestamp()
+    .setFooter(`Requested by ${message.author.tag}`, client.user.displayAvatarURL())
+    message.channel.send(invite);
+}
+
+module.exports.help = {
+    name: "invite"
+}
